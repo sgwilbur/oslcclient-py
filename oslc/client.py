e29dd20829548220bb323df1957ab4bc631bec1f
Sean G Wilbur <sgwilbur@gmail.com>
2012-08-01 14:04:06 -0500

    Adding some additional login logic to support logins to either Tomcat or  WAS. It appears that they behave slightly differently in how they cookies  and session ids are managed so this will probably have to be revisited.

--- oslc/client.py
+++ oslc/client.py
@@ -19,12 +19,16 @@ class OslcClient(object):
     def __init__(self, server_url, user, password):
         self.__DEBUG_LEVEL__ = 1
         self.base_url = server_url
-       
-        self.http = httplib2.Http( disable_ssl_certificate_validation=True )
-        self.http.follow_redirects = True
-        self.headers = {'content-type': 'text/xml', 'accept': 'application/x-oslc-cm-change-request+xml'}
+        self.auth_uri =  '/authenticated/identity'
         
-# todo: identify namespace mappings to spec/product versions       
+#        httplib2.debuglevel=0
+        self.http = httplib2.Http( disable_ssl_certificate_validation=True )
+#        self.http.follow_all_redircts = True
+        self.http.follow_redirects = True
+#        self.headers = {'content-type': 'text/xml', 'accept': 'application/x-oslc-cm-change-request+xml'}
+        self.headers = {'content-type': 'text/xml', 'accept': 'application/rdf+xml'}
+        
+# TODO: identify namespace mappings to spec/product versions       
         self.oslc_namespaces = {
                                 'rdf'  : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
                                 'dc'   : 'http://purl.org/dc/terms/',
@@ -48,7 +52,13 @@ class OslcClient(object):
                                  'atom'      : 'http://www.w3.org/2005/Atom', 
                                 }  
      
+        # Send initial challenge request for secured resource
         response, content = self.http.request( self.base_url + "/authenticated/identity", 'GET', headers=self.headers)
+        
+#        print(" >>> initial response: ", response )
+#        print(" >>>  initial content: ", content )
+        
+#        print( "Response to get authenticated/identity:" + response.__str__() )
         if response['x-com-ibm-team-repository-web-auth-msg'] != 'authrequired':
             raise Exception("Server error authenticating: " + response.__str__())
 
@@ -58,18 +68,46 @@ class OslcClient(object):
         # TODO: Post login information, Jazz uses j_security_check for FORM auth.
         response, content = self.http.request(self.base_url+'/authenticated/j_security_check' , 'POST',
                             headers=self.headers, body=urllib.urlencode({'j_username': user, 'j_password': password}))
+
+        # Special case, WAS returns the LPTA token here, instead of requiring the first get request to get the JSESSIONID
+#        print(" >>> login response: ", response )
+#        print(" >>>  login content: ", content )
+        if response.__contains__('set-cookie'):
+            print( ">>> Found the set-cookie header")
+            self.headers['cookie']=  response['set-cookie'] 
         
-        # Confirm that we are connected, and can grab the secure resource now      
+        
+        # Confirm that we are connected, and can grab the secure resource now
+#        print( " >>> cookie: ", self.headers['cookie'] )
         response, content = self.http.request( self.base_url + "/authenticated/identity", 'GET', headers=self.headers)
-        if response.__contains__('x-com-ibm-team-repository-web-auth-msg'):
+        
+#        print(" >>> check response: ", response )
+#        print(" >>>  check content: ", content )
+        
+        if response.__contains__('X-com-ibm-team-repository-web-auth-msg'):
+#            print( "Response: " + response.__str__() )
+#            print( "Content: " + content.__str__() )
             raise Exception("Login was not successful, server response: " + response.__str__() )
+##
+## FIXME:  Super hack so this can handle both Tomcat and WAS 
+##  It seems that we need to keep the cookie updated for Tomcat but for WAS we want to leave it as
+## is. Not sure how this works, so it seems like a hack to me for now.
+##
+        elif response.__contains__('set-cookie') and "LtpaToken2" not in self.headers['cookie'] :
+#            print( ">>> Found the set-cookie header")
+#            print( " >>> original  cookie: ", self.headers['cookie'] )
+#            print( " >>> new value cookie: ", response['set-cookie'])
+            self.headers['cookie']=  response['set-cookie']
+            
+        self.headers['accept'] = 'text/xml'
+        self.headers['content-type'] = 'text/xml'
 
 
     def __login__(self):
         # Grab secured resource to initiate login
         response, content = self.http.request( self.base_url + "/authenticated/identity", 'GET', headers=self.headers)
         
-        if response.__contains__('x-com-ibm-team-repository-web-auth-msg'):
+        if response.__contains__('X-com-ibm-team-repository-web-auth-msg'):
         
             if response['x-com-ibm-team-repository-web-auth-msg'] != 'authrequired':
                 raise Exception("Server error authenticating: " + response.__str__())
@@ -81,7 +119,7 @@ class OslcClient(object):
             # Confirm that we are connected, and can grab the secure resource now      
             response, content = self.http.request( self.base_url + "/authenticated/identity", 'GET', headers=self.headers)
             
-            if response.__contains__('x-com-ibm-team-repository-web-auth-msg'):
+            if response.__contains__('X-com-ibm-team-repository-web-auth-msg'):
                 raise Exception("Login was not successful, server response: " + response.__str__() )
 
 #
@@ -94,11 +132,23 @@ class OslcClient(object):
 #
 #  Generic helper to grab a URLs
 #
+    def authenticatedRequest( self, type, url, headers, content={} ):
+        print( 'unimplemented' )
+
+
     def get( self, url ):
         return  self.getH( url, self.headers )
 
     def getH( self, url, headers ):
+        print( " >>> getH headers")
+        pprint.pprint( headers )
         response, content = self.http.request(url, 'GET', headers=headers)
+        print( " >>> getH response")
+        pprint.pprint( response )
+
+        if response.__contains__('X-com-ibm-team-repository-web-auth-msg'):
+            raise Exception("Need to authenticate and try again.")
+        
         if response.status != 200:
             raise Exception("OslcClient response status != 200 !!!" + response.__str__() + 'content: ' + content.__str__() )
         return content
@@ -117,7 +167,10 @@ class OslcClient(object):
         headers = self.headers
 #        headers = {'Content-type': 'application/x-oslc-cm-change-request+xml', 'accept': 'text/xml'}
 
-        response, content = self.http.request(url, 'POST', contentXML, headers=headers)        
+        response, content = self.http.request(url, 'POST', contentXML, headers=headers)
+
+        if response.__contains__('X-com-ibm-team-repository-web-auth-msg'):
+            raise Exception("Need to authenticate and try again.")
 #        if response.status != 200:
 #            raise Exception("OslcClient response status != 200 !!!" + response.__str__())
         return content
